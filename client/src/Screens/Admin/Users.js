eef802f201b1d2e3c70b295a29c390fee01586c4
Nataly Villalba <alexandra191192@hotmail.com>
2023-03-27 21:20:16 -0400

    page structure (not done)

--- client/src/Screens/Admin/Users.js
+++ client/src/Screens/Admin/Users.js
@@ -2,9 +2,15 @@ import { Navigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import NavBar from "../../Components/NavBar";
 import { userState } from "../../globalstate";
+import "../../App.css"
 
 const Users = () => {
   const [user, setUser] = useRecoilState(userState);
+  const initialData = [
+      {name: "Chris Purnell", email: "uliajkas@jhosk.com", active: "yes", admin: "yes", status:"joined"},
+      {name: "Will Marttala", email: "uliajkas@jhosk.com", active: "yes", admin: "yes", status:"joined"},
+      {name: "Kenny Worth", email: "uliajkas@jhosk.com", active: "yes", admin: "yes", status:"joined"}
+  ]
 
   if (!user.isLoggedIn) {
     return <Navigate replace to="/" />;
@@ -13,7 +19,30 @@ const Users = () => {
       <div className="page">
         <NavBar />
         <div className="page-body">
-          <h1>Users</h1>
+          <h1>User Registry</h1>
+            <h2>A general view of all your members in your organization</h2>
+            <table  className="table">
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Active</th>
+                    <th>Admin</th>
+                    <th>Status</th>
+                </tr>
+                {initialData.map((val,key)=>{
+                return (
+                    <tr key={key}>
+                        <td>{val.name}</td>
+                        <td>{val.email}</td>
+                        <td>{val.active}</td>
+                        <td>{val.admin}</td>
+                        <td>{val.status}</td>
+                    </tr>
+                )})
+                }
+
+            </table>
+            <button>ADD USER</button>
         </div>
       </div>
     );
