11fc189b467b081e657e9fb01dd169e4032a3d1d
Darian Ngo <darianngo@gmail.com>
2023-03-28 06:15:09 -0500

    Implemented GET allAnnouncements

--- group-final/src/main/java/com/cooksys/groupfinal/controllers/AnnouncementController.java
+++ group-final/src/main/java/com/cooksys/groupfinal/controllers/AnnouncementController.java
@@ -1,8 +1,12 @@
 package com.cooksys.groupfinal.controllers;
 
+import java.util.Set;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cooksys.groupfinal.dtos.AnnouncementDto;
 import com.cooksys.groupfinal.services.AnnouncementService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,4 +18,8 @@ public class AnnouncementController {
 	
 	private final AnnouncementService announcementService;
 
+	@GetMapping
+	public Set<AnnouncementDto> getAllAnnouncements() {
+		return announcementService.getAllAnnouncements();
+	}
 }
--- group-final/src/main/java/com/cooksys/groupfinal/services/impl/AnnouncementServiceImpl.java
+++ group-final/src/main/java/com/cooksys/groupfinal/services/impl/AnnouncementServiceImpl.java
@@ -1,7 +1,13 @@
 package com.cooksys.groupfinal.services.impl;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 
+import com.cooksys.groupfinal.dtos.AnnouncementDto;
+import com.cooksys.groupfinal.mappers.AnnouncementMapper;
+import com.cooksys.groupfinal.repositories.AnnouncementRepository;
 import com.cooksys.groupfinal.services.AnnouncementService;
 
 import lombok.RequiredArgsConstructor;
@@ -9,5 +15,14 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class AnnouncementServiceImpl implements AnnouncementService {
+
+	private final AnnouncementRepository announcementRepository;
+	private final AnnouncementMapper announcementMapper;
+	
+	
+	@Override
+	public Set<AnnouncementDto> getAllAnnouncements() {
+		return announcementMapper.entitiesToDtos(new HashSet<>(announcementRepository.findAll()));
+	}
 
 }
