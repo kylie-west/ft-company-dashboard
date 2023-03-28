aca4fde6f7329baa62d25cf07efcec9f56b84438
Kylie West <kmichellewest@gmail.com>
2023-03-27 17:28:22 -0700

    Add form in CreateAnnounceModal + create reusable StyledTextField component for forms

--- client/src/Components/Modals/CreateAnnounceModal.js
+++ client/src/Components/Modals/CreateAnnounceModal.js
@@ -1,16 +1,67 @@
 import React, { useState } from "react";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import FormControl from "@mui/material/FormControl";
+import StyledTextField from "../StyledTextField";
 import SubmitButton from "../SubmitButton";
+import { announcementsState } from "../../globalstate";
 
+/**
+ * @todo Set current user as author
+ * @todo Close modal and reset modal state on submit
+ * @todo Update submit button cursor/hover styles
+ * @todo Preserve newlines?
+ * @todo Use actual backend API data
+ */
 const CreateAnnounceModal = () => {
-  function handleSubmit() {}
+	const [announcements] = useRecoilState(announcementsState);
+	const setAnnouncements = useSetRecoilState(announcementsState);
+	const [form, setForm] = useState({ title: "", message: "" });
 
-  return (
-    <div className="modal-body">
-      create announcement
-      <SubmitButton handleSubmit={handleSubmit} />
-    </div>
-  );
+	function handleChange(e) {
+		setForm({ ...form, [e.target.id]: e.target.value });
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		const newAnnouncement = {
+			id: Math.floor(Math.random() * 500000),
+			date: new Date(),
+			title: form.title,
+			message: form.message,
+			author: "Chris, CEO"
+		};
+		setAnnouncements([...announcements, newAnnouncement]);
+	}
+
+	return (
+		<div className="modal-body">
+			<FormControl style={formStyle}>
+				<StyledTextField
+					id="title"
+					value={form.title}
+					label="title"
+					variant="standard"
+					onChange={handleChange}
+				/>
+				<StyledTextField
+					id="message"
+					value={form.message}
+					label="message"
+					variant="standard"
+					multiline
+					onChange={handleChange}
+				/>
+				<SubmitButton handleSubmit={handleSubmit} />
+			</FormControl>
+		</div>
+	);
 };
 
 export default CreateAnnounceModal;
+
+const formStyle = {
+	display: "flex",
+	flexDirection: "column",
+	gap: "2rem",
+	width: "300px"
+};
