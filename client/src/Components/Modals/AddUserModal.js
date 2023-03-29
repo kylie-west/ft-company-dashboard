8c08aeca6d3470acfaa4cc949ef83c7e0b098f72
Nataly Villalba <alexandra191192@hotmail.com>
2023-03-28 22:17:05 -0400

    added modal form for user screen

--- client/src/Components/Modals/AddUserModal.js
+++ client/src/Components/Modals/AddUserModal.js
@@ -1,16 +1,74 @@
-import React, { useState } from "react";
-import { useRecoilState } from "recoil";
+import React from "react";
 import SubmitButton from "../SubmitButton";
+import FormControl from "@mui/material/FormControl";
+import StyledTextField from "../StyledTextField";
 
 const AddUserModal = () => {
-  function handleSubmit() {}
+    function handleSubmit() {
+    }
 
-  return (
-    <div className="modal-body">
-      add user
-      <SubmitButton handleSubmit={handleSubmit} />
-    </div>
-  );
+    return (
+        <div className="modal-body">
+            {/*<div className="modal-row">
+                <div>subdiv1</div>
+                <div>subdiv2</div>
+            </div>
+            <div>div2</div>
+            <div>div3</div>
+            <div className="modal-row">
+                <div>subdiv1</div>
+                <div>subdiv2</div>
+            </div>
+            <div>div5</div>
+            <div>div6</div>
+            <div>div7</div>*/}
+            <FormControl style={formStyle}>
+                <div className="modal-row">
+                    <StyledTextField
+                        variant="standard"
+                        placeholder="first name"
+                    />
+                    <StyledTextField
+                        variant="standard"
+                        placeholder="last name"
+                    />
+                </div>
+                <StyledTextField
+                    variant="standard"
+                    placeholder="email"
+                />
+                <StyledTextField
+                    variant="standard"
+                    placeholder="phone number"
+                />
+                <div className="modal-row">
+                    <StyledTextField
+                        variant="standard"
+                        placeholder="password"
+                    />
+                    <StyledTextField
+                        variant="standard"
+                        placeholder="confirm password"
+                    />
+                </div>
+            </FormControl>
+            <label>Make user an admin role?</label>
+            <select>
+                <option value="" disabled selected hidden>Pick an option</option>
+                <option>true</option>
+                <option>false</option>
+            </select>
+            <SubmitButton handleSubmit={handleSubmit}/>
+        </div>
+    );
 };
 
 export default AddUserModal;
+
+const formStyle = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "20px",
+    width: "clamp(20vw, 300px, 90vw)",
+    padding: "30px 30px 50px 30px"
+};
