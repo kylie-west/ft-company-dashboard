320199c8366c19183c6587374843375fcbb33ad9
Nataly Villalba <alexandra191192@hotmail.com>
2023-03-29 23:54:32 -0400

    made a method in user service to create a new user

--- client/src/Components/Modals/AddUserModal.js
+++ client/src/Components/Modals/AddUserModal.js
@@ -1,10 +1,68 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import SubmitButton from "../SubmitButton";
 import FormControl from "@mui/material/FormControl";
 import StyledTextField from "../StyledTextField";
+import {postUser} from "../../Services/users";
+import {useRecoilState} from "recoil";
+import {allUsersState, appState, modalState, userState} from "../../globalstate";
 
 const AddUserModal = () => {
-    function handleSubmit() {
+    const [app] = useRecoilState(appState);
+    const [user] = useRecoilState(userState);
+    const [modal, setModal] = useRecoilState(modalState);
+    const [users, setUsers] = useRecoilState(allUsersState);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [isEmpty, setIsEmpty] = useState(true);
+    const [attemptedSubmit, setAttemptedSubmit] = useState(false);
+
+    useEffect(() => {
+        firstName.length === 0 ||
+        lastName.length === 0 ||
+        email.length === 0 ||
+        phone.length === 0 ||
+        username.length === 0 ||
+        password.length === 0 ||
+        confirmPassword.length === 0
+            ? setIsEmpty(true)
+            : setIsEmpty(false);
+    }, [firstName, lastName, email, phone, username, password, confirmPassword]);
+
+    async function handleSubmit() {
+        setAttemptedSubmit(true);
+        if (isEmpty) return;
+        const addUserRequestDto = {
+            user: {
+                credentials: {
+                    username: username,
+                    password: password
+                },
+                profile: {
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    phone: phone
+                },
+                admin: isAdmin
+            },
+            credentials: {
+                username: user.username,
+                password: user.password,
+            }
+        }
+        console.log(addUserRequestDto);
+        const response = await postUser(
+            app.viewCompanyId,
+            {...addUserRequestDto}
+        );
+        setUsers([...users, response]);
+        setModal({isOpen: false, type: "", data: {}});
     }
 
     return (
@@ -26,38 +84,63 @@ const AddUserModal = () => {
                 <div className="modal-row">
                     <StyledTextField
                         variant="standard"
-                        placeholder="first name"
+                        id="user-first-name-input"
+                        label="First Name"
+                        error={attemptedSubmit && firstName.length === 0}
+                        onChange={(e) => setFirstName(e.target.value)}
                     />
                     <StyledTextField
                         variant="standard"
-                        placeholder="last name"
+                        id="user-last-name-input"
+                        label="Last Name"
+                        error={attemptedSubmit && lastName.length === 0}
+                        onChange={(e) => setLastName(e.target.value)}
                     />
                 </div>
                 <StyledTextField
                     variant="standard"
-                    placeholder="email"
+                    id="user-email-input"
+                    label="Email"
+                    error={attemptedSubmit && email.length === 0}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
                 <StyledTextField
                     variant="standard"
-                    placeholder="phone number"
+                    id="user-phone-input"
+                    label="Phone Number"
+                    error={attemptedSubmit && phone.length === 0}
+                    onChange={(e) => setPhone(e.target.value)}
+                />
+                <StyledTextField
+                    variant="standard"
+                    id="user-username-input"
+                    label="Username"
+                    error={attemptedSubmit && username.length === 0}
+                    onChange={(e) => setUsername(e.target.value)}
                 />
                 <div className="modal-row">
                     <StyledTextField
                         variant="standard"
-                        placeholder="password"
+                        id="user-password-input"
+                        label="Password"
+                        error={attemptedSubmit && password.length === 0}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                     <StyledTextField
                         variant="standard"
-                        placeholder="confirm password"
+                        id="user-confirm-password-input"
+                        label="Confirm Password"
+                        error={attemptedSubmit && confirmPassword.length === 0}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
                     />
                 </div>
             </FormControl>
-            <label>Make user an admin role?</label>
+            {/*<label>Make user an admin role?</label>
             <select>
                 <option value="" disabled selected hidden>Pick an option</option>
                 <option>true</option>
                 <option>false</option>
-            </select>
+            </select>*/}
             <SubmitButton handleSubmit={handleSubmit}/>
         </div>
     );
--- client/src/Services/users.js
+++ client/src/Services/users.js
@@ -1,13 +1,41 @@
 import api from "./api";
 
 export const login = async (username, password) => {
-  const response = await api.post("/users/login", {
-    username: username,
-    password: password,
-  });
-  return response.data;
+    const response = await api.post("/users/login", {
+        username: username,
+        password: password,
+    });
+    return response.data;
 };
 
 export const getCompanyUsers = async (companyId) => {
-  return api.get(`/company/${companyId}/users`);
+    return api.get(`/company/${companyId}/users`);
+};
+
+export const postUser = async (
+    companyId,
+    addUserRequestDto = {
+        user: {
+            credentials: {
+                username: newUsername,
+                password: newPassword
+            },
+            profile: {
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                phone: phone
+            },
+            admin: isAdmin
+        },
+        credentials: {
+            username: adminUsername,
+            password: adminPassword
+        }
+    }
+) => {
+    const response = await api
+        .post(`/users/${companyId}`, {addUserRequestDto})
+        .catch((err) => console.log(err));
+    return response.data;
 };
