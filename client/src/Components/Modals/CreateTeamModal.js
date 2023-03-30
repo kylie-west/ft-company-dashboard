523384ccb7da74ad8eadc992613a74551f678a94
Kylie West <kmichellewest@gmail.com>
2023-03-29 18:46:24 -0700

    Style create team modal

--- client/src/Components/Modals/CreateTeamModal.js
+++ client/src/Components/Modals/CreateTeamModal.js
@@ -6,7 +6,7 @@ import { getCompanyUsers } from "../../Services/users";
 import { postTeams } from "../../Services/teams";
 import { FormControl, Select, MenuItem } from "@mui/material";
 import StyledTextField from "../StyledTextField";
-import ClearIcon from '@mui/icons-material/Clear';
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 
 const CreateTeamModal = () => {
   const [app] = useRecoilState(appState);
@@ -35,33 +35,34 @@ const CreateTeamModal = () => {
     getAllUsers();
   }, []);
 
-  const options = availableUsers.map((member) => (
-    <MenuItem key={member.id} value={member.id}>
+  const options = availableUsers.map(member => (
+    <MenuItem key={member.id} value={member.id} style={{ fontSize: "1.6rem" }}>
       {member.profile.firstName + " " + member.profile.lastName}
     </MenuItem>
   ));
 
-  const style = {
-    backgroundColor: "white",
-    borderRadius: "6px",
-    fontSize: "16px",
-    minWidth: "120px",
-  };
-
   function handleChange(e) {
-    console.log(e)
+    console.log(e);
     const selectedId = e.target.value;
-    setSelectedUsers([...selectedUsers, availableUsers.find((user) => { 
-      return user.id === selectedId})]);
-    setAvailableUsers(availableUsers.filter(user => user.id !== selectedId))
+    setSelectedUsers([
+      ...selectedUsers,
+      availableUsers.find(user => {
+        return user.id === selectedId;
+      })
+    ]);
+    setAvailableUsers(availableUsers.filter(user => user.id !== selectedId));
   }
 
   function handleRemove(e) {
     console.log(e);
     const selectedId = e.id;
-    setAvailableUsers([...availableUsers, selectedUsers.find((user) => { 
-      return user.id === selectedId})]);
-    setSelectedUsers(selectedUsers.filter(user => user.id !== selectedId))
+    setAvailableUsers([
+      ...availableUsers,
+      selectedUsers.find(user => {
+        return user.id === selectedId;
+      })
+    ]);
+    setSelectedUsers(selectedUsers.filter(user => user.id !== selectedId));
   }
 
   async function handleSubmit() {
@@ -81,44 +82,90 @@ const CreateTeamModal = () => {
   }
 
   return (
-    <div className="modal-body">
-      create team
-      <StyledTextField
-        id="project-name-input"
-        label="Project Name"
-        variant="standard"
-        error={attemptedSubmit && name.length === 0}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <StyledTextField
-        id="description-input"
-        label="Description"
-        variant="standard"
-        multiline
-        error={attemptedSubmit && description.length === 0}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      {availableUsers.length > 0 && (
-        <FormControl sx={style}>
-          <Select value={""} onChange={(event) => handleChange(event)}>
+    <div className="modal-body create-team">
+      <h2 style={{ fontWeight: 300 }}>Create Team</h2>
+      <FormControl style={formStyle} size="small">
+        <StyledTextField
+          id="project-name-input"
+          label="Team Name"
+          variant="standard"
+          error={attemptedSubmit && name.length === 0}
+          onChange={e => setName(e.target.value)}
+          style={{ width: "100%" }}
+        />
+        <StyledTextField
+          id="description-input"
+          label="Description"
+          variant="standard"
+          multiline
+          error={attemptedSubmit && description.length === 0}
+          onChange={e => setDescription(e.target.value)}
+          style={{ width: "100%" }}
+        />
+        <label
+          style={{ fontSize: "2rem", color: "#deb992", marginTop: "20px" }}
+          for="members">
+          Select Members
+        </label>
+        {availableUsers.length > 0 && (
+          <Select
+            name="members"
+            value={""}
+            onChange={event => handleChange(event)}
+            sx={selectStyle}>
             {options}
-          </Select>  
-        </FormControl>
-      )}
+          </Select>
+        )}
+      </FormControl>
 
-      <div>
-        Select Team Members
-        {selectedUsers.map((u) => (
-          <div>
-            <span key={u.id}>{u.profile.firstName}</span>
-            <ClearIcon onClick={() => handleRemove(u)} /> 
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          marginBottom: "50px",
+          width: "200px"
+        }}>
+        {selectedUsers.map(u => (
+          <div style={selectedUserStyle}>
+            <div className="team-pill">
+              <span key={u.id}>
+                {u.profile.firstName + " " + u.profile.lastName[0] + "."}
+              </span>
+            </div>
+            <HighlightOffIcon
+              className="delete-member-icon"
+              onClick={() => handleRemove(u)}
+            />
           </div>
-
         ))}
       </div>
+
       <SubmitButton handleSubmit={handleSubmit} />
     </div>
   );
 };
 
 export default CreateTeamModal;
+
+const formStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "20px",
+  width: "clamp(20vw, 300px, 90vw)",
+  padding: "30px"
+};
+
+const selectStyle = {
+  width: "200px",
+  backgroundColor: "white",
+  borderRadius: "10px",
+  fontSize: "1.6rem"
+};
+
+const selectedUserStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "5px"
+};
