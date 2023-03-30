3d7c6953d1a01472f043368a0a83f9fa375db8cc
Kylie West <kmichellewest@gmail.com>
2023-03-29 19:29:45 -0700

    Style login page

--- client/src/Components/StyledTextField.js
+++ client/src/Components/StyledTextField.js
@@ -5,7 +5,8 @@ const StyledTextField = styled(TextField)({
   "& textarea": { color: "#fff", fontSize: "1.4rem", paddingBottom: "5px" },
   "& label": { color: "#1ba098", fontSize: "1.6rem" },
   "& label.Mui-focused": { color: "#deb992" },
-  "& .MuiInput-underline:after": { borderBottomColor: "#deb992" },
+  "& .MuiInput-underline:before": { borderBottomColor: "#1ba098" },
+  "& .MuiInput-underline:after": { borderBottomColor: "#deb992" }
 });
 
 export default StyledTextField;
